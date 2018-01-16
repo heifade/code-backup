fff12038c31978d62128fcaa2febf72469a6d7bb
heifade <heifade@126.com>
2018-01-16 17:33:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+code-backup
+
+
+[![NPM version][npm-image]][npm-url]
+[![NPM Downloads][downloads-image]][npm-url]
+[![Build Status](https://travis-ci.org/heifade/code-backup.svg?branch=master)](https://travis-ci.org/heifade/code-backup)
+[![Coverage Status](https://coveralls.io/repos/github/heifade/code-backup/badge.svg?branch=master)](https://coveralls.io/github/heifade/code-backup?branch=master)
+
+[npm-image]: https://img.shields.io/npm/v/code-backup.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/code-backup
+[downloads-image]: https://img.shields.io/npm/dm/code-backup.svg
+
+# 源代码及文档
+[源代码](https://github.com/heifade/code-backup)
+[开发文档](https://heifade.github.io/code-backup/)
+
+# 安装
+```bash
+npm install code-backup
+```
+
+# 介绍
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { option } from "yargs";
+import chalk from "chalk";
+import { spawnSync, SpawnSyncOptionsWithStringEncoding, execSync } from "child_process";
+
+let pars = option("s", {
+  alias: "r",
+  demand: false,
+  default: `C:/"Program Files"/WinRAR/WinRAR.exe`,
+  describe: "WinRAR程序的URL",
+  type: "string"
+})
+  .option("p", {
+    alias: "password",
+    demand: true,
+    default: "",
+    describe: "请输入压缩密码。",
+    type: "string"
+  })
+  .option("s", {
+    alias: "source",
+    demand: true,
+    // default: "",
+    describe: "输入源目录。",
+    type: "string"
+  })
+  .option("o", {
+    alias: "out",
+    demand: false,
+    default: "",
+    describe: "输入压缩包全名。",
+    type: "string"
+  }).argv;
+
+let options: SpawnSyncOptionsWithStringEncoding = {
+  encoding: "utf8",
+  cwd: __dirname,
+  stdio: [process.stdin, process.stdout, process.stderr]
+};
+
+let childProcess = spawnSync(`${pars.r}`, ["a", "-r", "-rv1", "-ibck", "-m5", "-or", "-agYYYYMMDDHHMMSS", `-hp${pars.p}`, ".rar", `${pars.s}`], options);
+
+console.log(childProcess);
